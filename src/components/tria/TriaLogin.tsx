08766f1f62192041a139aef622ac3088d943618d
AhaanSehgal <149166091+AhaanSehgal@users.noreply.github.com>
2023-12-01 15:09:38 +0530

    Update TriaLogin.tsx

--- src/components/tria/TriaLogin.tsx
+++ src/components/tria/TriaLogin.tsx
@@ -1,14 +1,14 @@
 //@ts-nocheck
 import dynamic from 'next/dynamic'
-import "@tria-sdk/authenticate/dist/index.css"
+import "authenticate-test-2/dist/index.css"
 const Application = dynamic(
-    () => import("@tria-sdk/authenticate"),
+    () => import("authenticate-test-2"),
     { ssr: false }
 )
 
 const TriaLogin = () => {
     return (
-         <Application
+        <Application
             logo="https://www.stackos.io/stackos-logo.svg"
             dappName="Stack OS"
             dappDomain={window.parent.origin}
