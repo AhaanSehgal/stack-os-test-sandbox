d6c83bd8be34d6f591078b5f6c84949390f06322
AhaanSehgal <149166091+AhaanSehgal@users.noreply.github.com>
2023-12-18 01:27:44 +0530

    Update TriaLogin.tsx

--- src/components/tria/TriaLogin.tsx
+++ src/components/tria/TriaLogin.tsx
@@ -1,6 +1,6 @@
 //@ts-nocheck
 import dynamic from 'next/dynamic'
-import "authenticate-test-2/dist/index.css"
+
 const Application = dynamic(
     () => import("authenticate-test-2"),
     { ssr: false }
@@ -8,15 +8,28 @@ const Application = dynamic(
 
 const TriaLogin = () => {
     return (
-        <Application
-            logo="https://www.stackos.io/stackos-logo.svg"
-            dappName="Stack OS"
-            dappDomain={window.parent.origin}
-            uiType={"yes"}
-            primaryColor="#AAFF00"
-            defaultChain="FUSE"
-            supportedChains={["FUSE", "POLYGON"]}
-        />
+        <div
+            style={{
+                position: 'fixed',
+                top: 0,
+                left: 0,
+                zIndex: 9999,
+            }}
+
+        >
+            <Application
+                logo="https://www.stackos.io/stackos-logo.svg"
+                dappName="Stack OS"
+                dappDomain="https://www.stackos.io"
+                uiType={'yes'}
+                triaStaging={true}
+                primaryColor="#AAFF00"
+                defaultChain="FUSE"
+                supportedChains={['FUSE', 'POLYGON', 'MUMBAI']}
+                buttonPosition={{ x: 200, y: 200 }}
+                darkMode={true}
+            />
+        </div>
     )
 }
 
